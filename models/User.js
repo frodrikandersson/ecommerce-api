75b393cf2ebb330c5ea1a794e667da2c445a08dd
Eric Mossberg <Emberg94@gmail.com>
2024-12-12 10:10:00 +0100

    Minor cleanup

--- models/User.js
+++ models/User.js
@@ -1,8 +1,5 @@
 const mongoose = require('mongoose');
 
-//might not need
-//const Schema = mongoose.Schema;
-
 const UserSchema = mongoose.Schema({
     name: {
         type: String,
@@ -21,8 +18,5 @@ const UserSchema = mongoose.Schema({
         required: false
     }
 });
-
-//might not need
-//const User = mongoose.model('User', UserSchema);
 
 module.exports = mongoose.model('User', UserSchema);
