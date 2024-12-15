d4841ef3e40f5d965d8d12398bed8496bee1b26d
Eric Mossberg <Emberg94@gmail.com>
2024-12-15 19:43:34 +0100

    Added login functionality, stored in session cookie. Also redirect after create user/login request.

--- routes/users.js
+++ routes/users.js
@@ -11,27 +11,38 @@ router.get('/', async (req, res) => {
     }
 });
 
-//Get one user by ID
-router.get('/:id', async (req, res) => {
+//Attempt login with email and password
+router.get('/:email/:password', async (req, res) => {
     try {
-        res.json(await User.find(req.params.id));
+        //find the user associated with the email
+        const guy = await User.findOne({email: req.params.email});
+        //check if password is a match, if so write cookie and redirect to page the request originated from
+        if(guy.password == req.params.password){
+            res.cookie("LoginCookieEcommerce", guy.id);
+            res.redirect(req.protocol + "://" + req.headers.host);
+        } else {
+            res.json("Login fail")
+        }
     } catch(error) {
         res.json({message: error});
     }
 });
 
-//POST a user
-router.post('/', async (req, res) => {
+//POST a user to database
+router.post('/add', async (req, res) => {
     try{
         const newUser = new User({
             name: req.body.name,
             email: req.body.email,
             password: req.body.password
         })
-        res.json(await newUser.save());
-    } catch (err){
-        res.json({message: err});
+        await newUser.save();
+        res.redirect(req.protocol + "://" + req.headers.host);
+    } catch (error){
+        res.json({message: error});
     }
 })
+
+router.checkout
 
 module.exports = router;
